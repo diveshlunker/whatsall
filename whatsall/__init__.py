ebb2d8ef86b5357e9e27237d78206235588a25b1
diveshlunker <diveshdj92@gmail.com>
2020-04-14 17:28:18 +0530

    Init file modified

--- whatsall/__init__.py
+++ whatsall/__init__.py
@@ -1,1 +1,1 @@
-from whatsall.py import start,helpdesk,message,send_message,numbers,number,speed,startWeb,stop
+from whatsall import start,helpdesk,message,send_message,numbers,number,speed,startWeb,stop
