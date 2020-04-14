bd9964a568578d2b824e2afeda59124842583bb0
diveshlunker <diveshdj92@gmail.com>
2020-04-14 19:36:57 +0530

    Stable Version Completed

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'whatsall',         
   packages = ['whatsall'],   
-  version = '0.0.6',      
+  version = '1',      
   license='MIT',        
   description = 'Automate sending messages on whatsapp to n number of users without even saving the number',   
   author = 'Divesh Lunker',             
   author_email = 'diveshdj92@gmail.com',      
   url = 'https://github.com/diveshlunker/whatsall',
-  download_url = 'https://github.com/diveshlunker/whatsall/archive/0.0.6.tar.gz',   
+  download_url = 'https://github.com/diveshlunker/whatsall/archive/1.tar.gz',   
   keywords = ['whatsapp', 'automated messaging', 'whatsapp web','whatspip'], 
   install_requires=[ 
           "selenium"
--- whatsall/whatsall.py
+++ whatsall/whatsall.py
@@ -46,7 +46,6 @@ def total_check(diff_location):
     else:
         check2 = driver_check(location)
         if(check2):
-            print("Opening Whatsapp...")
             startdriver(location)
         else:
             print("Download the Chrome driver exe file and unzip it and save it and then continue.")
@@ -58,25 +57,51 @@ def total_check(diff_location):
 def start(diff_location="C:\Drivers\chromeDriver\chromedriver.exe"):
     global total_check
     total_check = total_check(diff_location)
-    print(total_check)
 
 
         
 def helpdesk(self):
-    print("--------------------------------------------------------")
-    print("Welcome to Whatsapp Automated Messaging Library")
+    print("-------------------------------------------------------------------------")
+    print("Welcome to Whatsapp Automated Messaging Library - WhatsAll")
     print("Developed by Divesh Lunker")
-    print("Contribute @diveshlunker/whatsapp on github")
-    print("--------------------------------------------------------")
+    print("Contribute @diveshlunker/whatsall on github")
+    print("-------------------------------------------------------------------------")
     print("This library is totally unofficial and use it totally at your own risk")
     print("We do not take any responsibility for anything if goes wrong")
     print("--------------------------------------------------------------------------")
     print(""" 
     Follow the below instructions for easy use of this library
     
-    1. message() - Use this function in order to set the message to be sent
-                   A list should be sent containing the message with each value in list depicting a new line of message
-    2. 
+    1. start([location of chrome driver]) - use this function to start whatsall - If all the libraries are properly
+                                            installed, you can proceed further. The parameter is optional. 
+                                            The default location of driver is:- C:\Drivers\chromeDriver\chromedriver.exe
+                                            If you save the driver in the particular location, sending location of driver 
+                                            each time won't be required.
+    
+    2. startWeb(number,country_code) - send the number from which you wish to send for a trial purpose.
+    
+    3. message(list_message) - Use this function in order to set the message to be sent.
+                               A list should be sent containing the message with each value 
+                               in list depicting a new line of message
+    
+    4. numbers(list_numbers,country_code) - send the list of numbers whom you wish to send message with country_code
+                                            as second parameter
+                                            
+    5. number(number,country_code) - Instead of list, individual number can be sent.
+    
+    6. speed(time_in_seconds) - send time in seconds - increase if your internet is slow in order to send message properly.
+    
+    7. stop() - use this function to stop messaging and close the chrome driver
+    
+    ------------------------------------------------------------------------------------------------------------------------
+    
+    Additional Functionality will be added soon.
+    Feel free to contribute to the library.
+    
+    Contact developer @:-
+        Gmail - ndiveshjain@gmail.com
+        Github - @diveshlunker
+        Linkedin - @divesh-jain-652973145
     
     """)
     
@@ -161,5 +186,4 @@ def stop():
     global total_check
     driver.close()
     total_check = False
-start()
 
