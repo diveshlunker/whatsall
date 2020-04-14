a7a74631285cc086e5d2a133376e560842b47847
diveshlunker <diveshdj92@gmail.com>
2020-04-14 17:24:29 +0530

    adding release

--- whatsall/__init__.py
+++ whatsall/__init__.py
@@ -1,1 +1,1 @@
-import whatsall import start,helpdesk,message,send_message,numbers,number,speed,startWeb,stop
+from whatsall.py import start,helpdesk,message,send_message,numbers,number,speed,startWeb,stop
